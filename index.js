bf8a73df5fa9887c6c25e4967dc20b529dc81e56
eda46622-cell <eda46622@gmail.com>
2026-05-24 16:02:29 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const animalFacts = {
+        'Леопард': 'Леопарды — потрясающие верхолазы.Они могут затаскивать на деревья тяжелые туши животных (весом в три раза больше их самих), пряча их от гиен.',
+        'Енот': 'Лапы енотов содержат в тысячи раз больше осязательных рецепторов, чем руки человека. Именно поэтому они постоянно все трогают и «полощут» предметы в воде.',
+        'Тигр': 'Полосы на шкуре тигра уникальны, как отпечатки пальцев у людей. Двух одинаковых тигров в природе не существует.'
+    };
+
+    const backdrop = document.createElement('div');
+    backdrop.className = 'modal-backdrop';
+    backdrop.innerHTML = `
+        <div class="modal-window">
+            <button class="modal-close-btn">&times;</button>
+            <h3 class="modal-title" id="mTitle">Животное</h3>
+            <p class="modal-text" id="mText">Факт...</p>
+        </div>
+    `;
+    document.body.appendChild(backdrop);
+
+    const modalTitle = document.getElementById('mTitle');
+    const modalText = document.getElementById('mText');
+    const closeBtn = backdrop.querySelector('.modal-close-btn');
+х
+    const animalCards = document.querySelectorAll('.animal-column');
+
+    animalCards.forEach(card => {
+        card.addEventListener('click', () => {
+            const animalName = card.getAttribute('data-animal');
+            
+            if (animalFacts[animalName]) {
+                modalTitle.textContent = animalName;
+                modalText.textContent = animalFacts[animalName];
+                backdrop.classList.add('active');
+            }
+        });
+    });
+
+    closeBtn.addEventListener('click', () => backdrop.classList.remove('active'));
+    backdrop.addEventListener('click', (e) => {
+        if (e.target === backdrop) backdrop.classList.remove('active');
+    });
